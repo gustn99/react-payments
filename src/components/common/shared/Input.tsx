8ebf2dee4ed0d004e8b7c27a5d9f2c7a015aac45
gustn99 <azerosong@gmail.com>
2026-06-30 11:03:35 +0900

    feat: 공통 Input 컴포넌트 구현

---
+++ src/components/common/shared/Input.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { COLOR_PALETTE } from '../../../styles/colorPalette.ts';
+import { TYPOGRAPHY } from '../../../styles/typography.ts';
+
+const Input = styled.input`
+  ${TYPOGRAPHY.body2};
+  width: auto;
+  padding: 8px;
+  border: 1px solid ${COLOR_PALETTE.gray400};
+  border-radius: 2px;
+
+  :focus {
+    border: 1px solid ${COLOR_PALETTE.black};
+  }
+
+  ::placeholder {
+    color: ${COLOR_PALETTE.gray400};
+  }
+`;
+
+export default Input;
