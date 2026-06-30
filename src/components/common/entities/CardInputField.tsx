0e289f5611e21543fa2238b9ccd17c0ba9186599
gustn99 <azerosong@gmail.com>
2026-06-30 11:54:50 +0900

    feat: 공통 CardInputField 컴포넌트 구현

---
+++ src/components/common/entities/CardInputField.tsx
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+import { type PropsWithChildren } from 'react';
+import { COLOR_PALETTE } from '../../../styles/colorPalette.ts';
+import Spacing from '../shared/Spacing.tsx';
+import Text from '../shared/Text.tsx';
+
+type CardInputFieldProps = PropsWithChildren<{
+  title: string;
+  caption?: string;
+  errorText?: string;
+}>;
+
+export default function CardInputField({ title, caption, errorText, children }: CardInputFieldProps) {
+  return (
+    <Wrapper>
+      <Title as="h3" typograph="title1">
+        {title}
+      </Title>
+      {caption && (
+        <>
+          <Spacing direction="vertical" size={4} />
+          <Text typograph="caption1" color={COLOR_PALETTE.gray400}>
+            {caption}
+          </Text>
+        </>
+      )}
+      <Spacing direction="vertical" size={16} />
+      {children}
+      <Spacing direction="vertical" size={8} />
+      <ErrorText as="div" typograph="caption1" color={COLOR_PALETTE.red}>
+        {errorText}
+      </ErrorText>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div``;
+
+const Title = styled(Text)`
+  padding-block: 2px;
+`;
+
+const ErrorText = styled(Text)`
+  width: auto;
+  height: 14px;
+`;
