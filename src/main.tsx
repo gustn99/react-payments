be18294da30d0efb9fc2b77ddb03c0a866fe40cf
gustn99 <azerosong@gmail.com>
2026-06-30 11:52:29 +0900

    chore: 폰트 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/noto-sans/500.css';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/700.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
 import './styles/index.css';
 import App from './App.tsx';
 
