d69098e8453108f59b8962c3979fc3061b2d0aa3
gustn99 <azerosong@gmail.com>
2026-07-03 12:04:24 +0900

    feat: CvcInputField에 useCardForm 적용

--- src/components/features/CvcInputField.tsx
+++ src/components/features/CvcInputField.tsx
@@ -1,16 +1,19 @@
+import useCardForm from '../../hooks/useCardForm.ts';
 import CardInputField from '../common/entities/CardInputField.tsx';
 import NumberInput from '../common/shared/NumberInput.tsx';
 import Spacing from '../common/shared/Spacing.tsx';
 import Text from '../common/shared/Text.tsx';
 
 export default function CvcInputField() {
+  const { register, errors } = useCardForm();
+
   return (
-    <CardInputField title="CVC 번호를 입력해 주세요">
+    <CardInputField title="CVC 번호를 입력해 주세요" errorText={errors.cvc}>
       <Text as="label" typograph="body1">
-        유효기간
+        CVC
       </Text>
       <Spacing direction="vertical" size={8} />
-      <NumberInput placeholder="123" maxLength={3} />
+      <NumberInput placeholder="123" maxLength={3} {...register('cvc')} />
     </CardInputField>
   );
 }
