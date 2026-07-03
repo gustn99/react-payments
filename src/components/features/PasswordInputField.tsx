69ec3e936ff0dd3aea8b62694281f0061b0dd00a
gustn99 <azerosong@gmail.com>
2026-07-03 12:04:48 +0900

    feat: PasswordInputField에 useCardForm 적용

--- src/components/features/PasswordInputField.tsx
+++ src/components/features/PasswordInputField.tsx
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+import useCardForm from '../../hooks/useCardForm.ts';
 import CardInputField from '../common/entities/CardInputField.tsx';
 import NumberInput from '../common/shared/NumberInput.tsx';
 import Spacing from '../common/shared/Spacing.tsx';
 import Text from '../common/shared/Text.tsx';
 
 export default function PasswordInputField() {
-  const [value, setValue] = useState('');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
+  const { register, errors } = useCardForm();
 
   return (
-    <CardInputField title="비밀번호를 입력해 주세요" caption="앞의 2자리를 입력해주세요">
+    <CardInputField 
+      title="비밀번호를 입력해 주세요" 
+      caption="앞의 2자리를 입력해주세요"
+      errorText={errors.password}
+    >
       <Text as="label" typograph="body1">
         비밀번호 앞 2자리
       </Text>
       <Spacing direction="vertical" size={8} />
-      <NumberInput placeholder="**" value={value} onChange={handleChange} maxLength={2} />
+      <NumberInput type="password" placeholder="**" maxLength={2} {...register('password')} />
     </CardInputField>
   );
 }
