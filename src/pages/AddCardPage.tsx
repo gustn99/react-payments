ed4a63eda317eb5bddfbb376631997e09882de24
gustn99 <azerosong@gmail.com>
2026-07-06 16:15:02 +0900

    feat: AddCardPage 상단 여백 추가

--- src/pages/AddCardPage.tsx
+++ src/pages/AddCardPage.tsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
+import Spacing from '../components/common/shared/Spacing.tsx';
 import CardForm from '../components/features/CardForm.tsx';
 import { FormProvider } from '../contexts/FormContext.tsx';
 
 export default function AddCardPage() {
   return (
     <PageLayout>
+      <Spacing direction="vertical" size={77} />
       <FormProvider>
         <CardForm />
       </FormProvider>
