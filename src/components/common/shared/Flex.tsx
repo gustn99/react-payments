e88f91489897a0a1bb8dd9705b5129c88a047fec
gustn99 <azerosong@gmail.com>
2026-07-06 16:14:00 +0900

    feat: Flex 컴포넌트에 정렬 props 추가

--- src/components/common/shared/Flex.tsx
+++ src/components/common/shared/Flex.tsx
@@ -1,14 +1,18 @@
 import styled from '@emotion/styled';
 
 interface FlexProps {
-  direction?: 'row' | 'column';
+  direction?: React.CSSProperties['flexDirection'];
   gap?: number;
+  justify?: React.CSSProperties['justifyContent'];
+  align?: React.CSSProperties['alignItems'];
 }
 
 const Flex = styled.div<FlexProps>`
   display: flex;
   flex-direction: ${({ direction = 'row' }) => direction};
   gap: ${({ gap = 0 }) => `${gap}px`};
+  justify-content: ${({ justify = 'flex-start' }) => justify};
+  align-items: ${({ align = 'stretch' }) => align};
 `;
 
 export default Flex;
