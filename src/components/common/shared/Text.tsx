6a4231377c5c911a9e1eed5ef5f3b6945438d378
gustn99 <azerosong@gmail.com>
2026-07-06 16:14:14 +0900

    feat: Text 컴포넌트에 width props 추가

--- src/components/common/shared/Text.tsx
+++ src/components/common/shared/Text.tsx
@@ -6,11 +6,13 @@ export type TypographyVariant = keyof typeof TYPOGRAPHY;
 interface TextStyleProps {
   typograph?: TypographyVariant;
   color?: string;
+  width?: number;
 }
 
 const Text = styled.span<TextStyleProps>`
   ${({ typograph = 'body1' }) => TYPOGRAPHY[typograph]}
   color: ${({ color = 'inherit' }) => color};
+  width: ${({ width }) => (width ? `${width}px` : 'auto')};
   vertical-align: middle;
 `;
 
