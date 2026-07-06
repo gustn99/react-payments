23d3bacb20cb079cea4a42e6b513ddbcaf5c70aa
gustn99 <azerosong@gmail.com>
2026-07-06 09:59:11 +0900

    feat: AddCardPage에 CardForm 적용

--- src/pages/AddCardPage.tsx
+++ src/pages/AddCardPage.tsx
@@ -1,27 +1,12 @@
 import styled from '@emotion/styled';
-import Spacing from '../components/common/shared/Spacing.tsx';
-import CardCompanyInputField from '../components/features/CardCompanyInputField.tsx';
-import CardNumbersInputField from '../components/features/CardNumbersInputField.tsx';
-import CvcInputField from '../components/features/CvcInputField.tsx';
-import ExpirationDateInputField from '../components/features/ExpirationDateInputField.tsx';
-import PasswordInputField from '../components/features/PasswordInputField.tsx';
+import CardForm from '../components/features/CardForm.tsx';
 import { FormProvider } from '../contexts/FormContext.tsx';
 
 export default function AddCardPage() {
   return (
     <PageLayout>
       <FormProvider>
-        <Form>
-          <PasswordInputField />
-          <Spacing direction="vertical" size={16} />
-          <CvcInputField />
-          <Spacing direction="vertical" size={16} />
-          <ExpirationDateInputField />
-          <Spacing direction="vertical" size={16} />
-          <CardCompanyInputField />
-          <Spacing direction="vertical" size={16} />
-          <CardNumbersInputField />
-        </Form>
+        <CardForm />
       </FormProvider>
     </PageLayout>
   );
@@ -30,5 +15,3 @@ export default function AddCardPage() {
 const PageLayout = styled.div`
   padding-inline: 30px;
 `;
-
-const Form = styled.form``;
