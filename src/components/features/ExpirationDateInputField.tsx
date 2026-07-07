6592fa6a6d6186fa26c6a8ead94b79d540769a58
gustn99 <azerosong@gmail.com>
2026-07-07 11:54:19 +0900

    feat: ExpirationDate 필드에 다중 인풋 로직 적용

--- src/components/features/ExpirationDateInputField.tsx
+++ src/components/features/ExpirationDateInputField.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 import useCardForm from '../../hooks/useCardForm.ts';
+import { useMultipleInput } from '../../hooks/useMultipleInput.ts';
 import { validateExpirationDateMonth, validateExpirationDateYear } from '../../lib/validateForm.ts';
 import CardInputField from '../common/entities/CardInputField.tsx';
 import Flex from '../common/shared/Flex.tsx';
@@ -12,7 +13,36 @@ interface ExpirationDateInputFieldProps {
 }
 
 export default function ExpirationDateInputField({ onComplete }: ExpirationDateInputFieldProps) {
+  const { registerInputRef, handleKeyDown, moveToNext } = useMultipleInput();
+
   const { register, errors } = useCardForm();
+
+  const {
+    ref: monthRef,
+    onChange: onMonthChange,
+    ...monthProps
+  } = register('expirationMonth', { validate: validateExpirationDateMonth });
+  const {
+    ref: yearRef,
+    onChange: onYearChange,
+    ...yearProps
+  } = register('expirationYear', { validate: validateExpirationDateYear, onSuccess: onComplete });
+
+  const handleMonthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onMonthChange(e.target.value);
+
+    if (e.target.value.length === e.target.maxLength) {
+      moveToNext(0);
+    }
+  };
+
+  const handleYearChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onYearChange(e.target.value);
+
+    if (e.target.value.length === e.target.maxLength) {
+      moveToNext(1);
+    }
+  };
 
   return (
     <CardInputField
@@ -27,14 +57,26 @@ export default function ExpirationDateInputField({ onComplete }: ExpirationDateI
         <Spacing direction="vertical" size={8} />
         <Flex gap={8}>
           <NumberInput
+            {...monthProps}
+            ref={(refNode) => {
+              monthRef(refNode);
+              registerInputRef(0)(refNode);
+            }}
+            onChange={handleMonthChange}
+            onKeyDown={handleKeyDown(0)}
             placeholder="MM"
             maxLength={2}
-            {...register('expirationMonth', { validate: validateExpirationDateMonth })}
           />
           <NumberInput
+            {...yearProps}
+            ref={(refNode) => {
+              yearRef(refNode);
+              registerInputRef(1)(refNode);
+            }}
+            onChange={handleYearChange}
+            onKeyDown={handleKeyDown(1)}
             placeholder="YY"
             maxLength={2}
-            {...register('expirationYear', { validate: validateExpirationDateYear, onSuccess: onComplete })}
           />
         </Flex>
       </Fieldset>
