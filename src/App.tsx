860015b096a6b0a7401fbf5a82e2bffee6820dfa
gustn99 <azerosong@gmail.com>
2026-07-08 11:06:26 +0900

    feat: 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import AddCardPage from './pages/AddCardPage.tsx';
+import AddCardCompletePage from './pages/AddCardCompletePage.tsx';
 
 function App() {
   return (
-    <>
-      <AddCardPage />
-    </>
+    <BrowserRouter basename="/react-payments">
+      <Routes>
+        <Route path="/" element={<Navigate to="/card/add" replace />} />
+        <Route path="/card/add" element={<AddCardPage />} />
+        <Route path="/card/add/complete" element={<AddCardCompletePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
