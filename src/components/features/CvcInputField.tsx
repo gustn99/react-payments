982079bee10679399a35eb1e07f87865fa6355a7
gustn99 <azerosong@gmail.com>
2026-07-19 22:59:22 +0900

    fix: cvc maxLength 매직넘버 제거

--- src/components/features/CvcInputField.tsx
+++ src/components/features/CvcInputField.tsx
@@ -1,18 +1,22 @@
-import useCardForm from '@/hooks/useCardForm.ts';
-import { validateCvc } from '@/lib/validateForm.ts';
 import CardInputField from '@/components/common/entities/CardInputField.tsx';
 import NumberInput from '@/components/common/shared/NumberInput.tsx';
 import Spacing from '@/components/common/shared/Spacing.tsx';
 import Text from '@/components/common/shared/Text.tsx';
+import { CARD_RULES } from '@/constants/cardRules.ts';
+import useCardForm from '@/hooks/useCardForm.ts';
+import { getCardBrand } from '@/lib/getCardBrand.ts';
+import { validateCvc } from '@/lib/validateForm.ts';
 
 interface CvcInputFieldProps {
   onComplete?: () => void;
 }
 
 export default function CvcInputField({ onComplete }: CvcInputFieldProps) {
-  const { register, errors, touched } = useCardForm();
+  const { register, values, errors, touched } = useCardForm();
   const errorText = (touched.cvc && errors.cvc) || '';
   const isError = Boolean(errorText);
+
+  const cardBrand = getCardBrand(values.cardNumbers ?? '');
 
   return (
     <CardInputField title="CVC 번호를 입력해 주세요" errorText={errorText}>
@@ -23,7 +27,7 @@ export default function CvcInputField({ onComplete }: CvcInputFieldProps) {
       <NumberInput
         autoFocus
         placeholder="123"
-        maxLength={3}
+        maxLength={CARD_RULES[cardBrand].cvcLength}
         isError={isError}
         {...register('cvc', {
           validate: (value, values) => validateCvc(value, values.cardNumbers ?? ''),
